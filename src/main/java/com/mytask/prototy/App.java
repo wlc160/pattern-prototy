8cefe9b16e177ff7ca968176a9023abb843d9888
wlc160 <1602251271@qq.com>
2019-10-11 08:54:06 +0800

    first

---
+++ src/main/java/com/mytask/prototy/App.java
@@ -0,0 +1,13 @@
+package com.mytask.prototy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/mytask/prototy/staticPrototy/Prototype.java
@@ -0,0 +1,15 @@
+package com.mytask.prototy.staticPrototy;
+
+/**
+ * 工程名:pattern-prototy
+ * 包名:com.mytask.prototy.staticPrototy
+ * 文件名:Prototype
+ * description:
+ *
+ * @author lcwen
+ * @version V1.0: Prototype.java 2019/10/11 8:30
+ **/
+public interface Prototype {
+
+    Prototype clone();
+}
